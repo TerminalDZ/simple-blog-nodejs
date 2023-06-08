d2a4405b096c51289ff7e71d67e1d17e788e4cc0
Idriss Boukmouche <69165137+TerminalDZ@users.noreply.github.com>
2023-06-08 03:14:57 +0100

    up
    up

---
+++ blogdbh.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 08, 2023 at 04:12 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blogdbh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(15) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(1500) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `content`, `image`) VALUES
+(10, 'idsdf', 'dsfsdf', '1686189965540.jpg'),
+(11, 'fdg', 'fdgdfg', '1686190224003.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override'); 
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method')); 
+app.use(express.static('uploads'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'blogdbh'
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM articles', (error, results) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    res.render('index', { articles: results });
+  });
+});
+
+app.get('/articles/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/articles', upload.single('image'), (req, res) => {
+  const { title, content } = req.body;
+  const image = req.file ? req.file.filename : null;
+  connection.query('INSERT INTO articles (title, content, image) VALUES (?, ?, ?)', [title, content, image], (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    res.redirect('/');
+  });
+});
+
+app.get('/articles/:id', (req, res) => {
+  const articleId = req.params.id;
+  connection.query('SELECT * FROM articles WHERE id = ?', [articleId], (error, results) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    res.render('show', { article: results[0] });
+  });
+});
+
+app.delete('/articles/:id', (req, res) => { 
+  const articleId = req.params.id;
+  connection.query('DELETE FROM articles WHERE id = ?', [articleId], (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    res.redirect('/');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
